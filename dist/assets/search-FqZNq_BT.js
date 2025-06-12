1036e9d5174b22ac61c9cc40d1befdc9b740f602
Charles Law <calaw726@gmail.com>
2025-06-12 19:09:58 -0400

    FIX: SEO optimization with H1 headers

--- dist/assets/search-FqZNq_BT.js
+++
@@ -1,1 +0,0 @@
-import{b as a,i,o as e,h as n}from"./common-MJg3Cdk6.js";a();document.addEventListener("DOMContentLoaded",()=>{i("search"),e()});n();
---
+++ dist/assets/search-pyFuTLdm.js
@@ -0,0 +1,1 @@
+import{b as a,i,o as e,h as n}from"./common-CKLXUwgl.js";a();document.addEventListener("DOMContentLoaded",()=>{i("search"),e()});n();
