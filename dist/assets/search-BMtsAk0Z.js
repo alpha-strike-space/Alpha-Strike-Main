28ac3c174cf2202da420e3c0c0b988dfe708d338
Charles Law <calaw726@proton.me>
2025-06-15 18:55:31 -0400

    STYLE: Navbar optimizations for mobile

---
+++ dist/assets/search-BMtsAk0Z.js
@@ -0,0 +1,1 @@
+import{b as a,i,o as e,h as n}from"./common-sdwmEAW7.js";a();document.addEventListener("DOMContentLoaded",()=>{i("search"),e(),n()});
--- dist/assets/search-BixJ8O94.js
+++
@@ -1,1 +0,0 @@
-import{b as a,i,o as e,h as n}from"./common-C8-NmTGg.js";a();document.addEventListener("DOMContentLoaded",()=>{i("search"),e(),n()});
