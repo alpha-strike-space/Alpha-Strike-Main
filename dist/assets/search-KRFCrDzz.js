6b48ca7bdde76b5123a8356877c56213762b784a
Charles Law <calaw726@gmail.com>
2025-07-18 09:30:48 -0400

    CSS: Added padding to logo.

--- dist/assets/search-KRFCrDzz.js
+++
@@ -1,1 +0,0 @@
-import{i,p as a}from"./common-6Gyf7xGW.js";import{s as e,h as o}from"./loadingOverlay-Jo2eS91N.js";e();document.addEventListener("DOMContentLoaded",()=>{i("search"),a(),o()});
---
+++ dist/assets/search-UHOiiIzO.js
@@ -0,0 +1,1 @@
+import{i,p as a}from"./common-C_MkXPgi.js";import{s as e,h as o}from"./loadingOverlay-1zJWcMNp.js";e();document.addEventListener("DOMContentLoaded",()=>{i("search"),a(),o()});
